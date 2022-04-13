ac7905bb1e120de56d5c61b57151202357bc5968
Ivan Cheng <ivanchengzhongyan@gmail.com>
2022-04-13 22:51:31 +0800

    second commit

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+
+const About = () => {
+    return (
+        <div className="container about-page">
+            
+        </div>
+    )
+}
+
+export default About;
